8323a95b8079af8a01db602d00f835b7e4af9d07
MRamadan2511 <m.ramadan2511@gmail.com>
2023-11-03 22:26:13 +0200

    Adding company & Department Models

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Ticket
+from .models import Company, Department, Tag, Warehouse, Ticket
 
+admin.site.register(Company)
+admin.site.register(Department)
+admin.site.register(Tag)
+admin.site.register(Warehouse)
 admin.site.register(Ticket)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,47 @@
 from django.db import models
 
+
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Department(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_departments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return f"{self.name}  ->  {self.company}"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_tags')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}  ->  {self.company}"
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_warehouses')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}  ->  {self.company}"
+
+
 class Ticket(models.Model):
     description = models.TextField('Description', blank=True, null=True)
     post_image= models.ImageField(upload_to='image/post' ,blank=True, null=True,)
