d21ccd050b536f2f6bf58f6aa8a2aabafcf91763
MRamadan2511 <m.ramadan2511@gmail.com>
2024-01-02 01:28:04 +0200

    dashboard navbar

--- base/urls.py
+++ base/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('home/', views.location_inbox, name='home'),
     path('ticket_create/', views.ticket_create, name='ticket_create'),
     path('ticket_detail/<int:pk>/', views.ticket_detail, name='ticket_detail'),
-    path('ticket_detail/<int:pk>/add_comment', views.add_comment, name='add_comment'),
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('ticket_detail/<int:pk>/add_comment/', views.add_comment, name='add_comment'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 
     path('login/', views.login, name='login'),
     path('login/agent/', AgentLoginView.as_view(), name='agent_login'),
